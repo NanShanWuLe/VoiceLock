fae7a836423d2c95719befda6a0fbecf4529ab05
xiaozuo <962894439@qq.com>
2021-12-22 22:23:26 +0800

    add viewpager and fragment
    Signed-off-by: xiaozuo <962894439@qq.com>

---
+++ app/src/main/java/com/graduationproject/voicelock/Adapter/VPAdapter.java
@@ -0,0 +1,44 @@
+package com.graduationproject.voicelock.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VPAdapter extends FragmentStateAdapter {
+    private List<Class> fragments;
+
+    public VPAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+        if (fragments == null) {
+            fragments = new ArrayList<>();
+        }
+    }
+
+    public void addFragment(Fragment fragment) {
+        if (fragments != null) {
+            fragments.add(fragment.getClass());
+        }
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        try {
+            return (Fragment) fragments.get(position).newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
--- app/src/main/java/com/graduationproject/voicelock/MainActivity.java
+++ app/src/main/java/com/graduationproject/voicelock/MainActivity.java
@@ -1,8 +1,18 @@
 package com.graduationproject.voicelock;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changStatusIconCollor(true);
     }
+
+    public void changStatusIconCollor(boolean setDark) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            View decorView = window.getDecorView();
+            if(decorView != null){
+                int vis = decorView.getSystemUiVisibility();
+                if(setDark){
+                    window.setStatusBarColor(getResources().getColor(R.color.white));
+                    vis |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                } else{
+                    window.setStatusBarColor(getResources().getColor(R.color.black));
+                    vis &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                }
+                decorView.setSystemUiVisibility(vis);
+            }
+        }
+    }
+    
+    
 }
